8b4acab61985298c50597a12779a148c245014bf
trupti vaghasiya <ican.trupti@gmail.com>
2017-05-24 16:45:34 +0530

    Add  folder

---
+++ app/Default2.aspx.cs
@@ -0,0 +1,39 @@
+﻿using ASPNetMultiLanguage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class app_Default2 : ASPNetMultiLanguage.BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        #region--Show/hide language link
+        if (!string.IsNullOrEmpty(Convert.ToString(Session["lang"])))
+        {
+            //if (Convert.ToString(Session["lang"]) == "en")
+            //{
+            // //   linkVietnameseLang.Visible = true;
+            // //   linkEnglishLang.Visible = false;
+            //}
+            //else
+            //{
+            //    //   linkEnglishLang.Visible = true;
+            //    //   linkVietnameseLang.Visible = false;
+            //    //   Response.Redirect("~/HR/Currency.aspx");
+            //}
+            string value = Session["lang"].ToString();
+            BasePage objBasePage = new BasePage();
+            objBasePage.InitializeCulture1(value);
+            string LastUrl = Request.UrlReferrer.ToString();
+            Response.Redirect(LastUrl);
+        }
+        else
+        {
+           
+        }
+        #endregion--
+    }
+}
